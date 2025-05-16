14669b8141ec20f3f04e44748744cc021ac4544b
beshir <youssefahmedbeshir@gmail.com>
2025-05-16 18:19:18 +0300

    Seeker search

---
+++ recruitment/SeekerHome.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace recruitment
+{
+    partial class SeekerHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchTextHolder = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find your suitable job, easily";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // searchTextHolder
+            // 
+            this.searchTextHolder.Location = new System.Drawing.Point(182, 83);
+            this.searchTextHolder.Name = "searchTextHolder";
+            this.searchTextHolder.Size = new System.Drawing.Size(376, 22);
+            this.searchTextHolder.TabIndex = 1;
+            this.searchTextHolder.Text = "Search by job title, loaction or keywords (e.g. Sales in Cairo)";
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(564, 83);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 2;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Location = new System.Drawing.Point(182, 140);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.RowHeadersWidth = 51;
+            this.dgvResults.RowTemplate.Height = 24;
+            this.dgvResults.Size = new System.Drawing.Size(457, 284);
+            this.dgvResults.TabIndex = 3;
+            // 
+            // SeekerHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvResults);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.searchTextHolder);
+            this.Controls.Add(this.label1);
+            this.Name = "SeekerHome";
+            this.Text = "SeekerHome";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchTextHolder;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.DataGridView dgvResults;
+    }
+}
---
+++ recruitment/SeekerHome.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace recruitment
+{
+    public partial class SeekerHome : Form
+    {
+        public SeekerHome()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            string searchText = searchTextHolder.Text.Trim();
+
+            List<Vacancy> allJobs = GetAllJobs();
+
+            double threshold = 0.5;
+
+            var filteredJobs = allJobs.Where(job =>
+                Similarity(searchText, job.Title) >= threshold ||
+                Similarity(searchText, job.Location) >= threshold ||
+                Similarity(searchText, job.Field) >= threshold
+            ).ToList();
+
+            dgvResults.DataSource = filteredJobs;
+        }
+
+        private double Similarity(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2))
+                return 0.0;
+
+            var pairs1 = WordLetterPairs(s1.ToUpper());
+            var pairs2 = WordLetterPairs(s2.ToUpper());
+            int intersection = 0;
+            int totalPairs = pairs1.Count + pairs2.Count;
+
+            foreach (var pair1 in pairs1)
+            {
+                for (int j = 0; j < pairs2.Count; j++)
+                {
+                    if (pair1 == pairs2[j])
+                    {
+                        intersection++;
+                        pairs2.RemoveAt(j);
+                        break;
+                    }
+                }
+            }
+            return (2.0 * intersection) / totalPairs;
+        }
+
+        private List<string> WordLetterPairs(string str)
+        {
+            var allPairs = new List<string>();
+            string[] words = str.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrEmpty(word))
+                {
+                    var pairsInWord = LetterPairs(word);
+                    allPairs.AddRange(pairsInWord);
+                }
+            }
+            return allPairs;
+        }
+
+        private List<string> LetterPairs(string str)
+        {
+            var pairs = new List<string>();
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                pairs.Add(str.Substring(i, 2));
+            }
+            return pairs;
+        }
+
+        private List<Vacancy> GetAllJobs()
+        {
+            var jobs = new List<Vacancy>();
+            string connStr = ConfigurationManager.ConnectionStrings["recruiment"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                // TODO: replace with the actual database
+                string query = "SELECT Id, Title, Location, Field FROM Vacancies";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        jobs.Add(new Vacancy
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            Location = reader.GetString(2),
+                            Field = reader.GetString(3)
+                        });
+                    }
+                }
+            }
+            return jobs;
+        }
+
+        class Vacancy
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Location { get; set; }
+            public string Field { get; set; }
+        }
+    }
+}
+
+
+
+
